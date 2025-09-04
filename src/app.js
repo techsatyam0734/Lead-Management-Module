7b5cbfbbbbd09a13700f8d4cc906558c56696b34
Satyam Tiwari <satyamtiwari0734@gmail.com>
2025-09-04 20:00:30 +0530

    changes in formatting

--- src/app.js
+++ src/app.js
@@ -1,40 +1,16 @@
-import express from "express"
+import express from "express";
 
+const app = express();
 
-const app = express()
-
-
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //routes
-import leadRouter from "./routes/lead.routes.js"
+import leadRouter from "./routes/lead.routes.js";
 
 //routes declaration
-app.use("/api/v1/leads",leadRouter)
-
+app.use("/api/v1/leads", leadRouter);
 
 // http://localhost:3000/api/v1/leads/addLead
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-export {app}
+export { app };
